0b9f22e22271bacaee2f2f37278c6cd7c7c6f6a4
khangnguyenba75-ctrl <khangnguyenba75@gmail.com>
2026-06-24 12:01:57 +0700

    Fix syntax error in setInterval function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Saved Game
+// Version 1.2
+
 setInterval(() => {
-    console.log("🔴 LIVE UPDATE: " + new Date().toLocaleTimeString());
+console.log(
+"🔴 LIVE UPDATE: "
++ new Date().toLocaleTimeString()
+);
 }, 2000);
